b3ada8a109a6d8e2ac2addc36ef00a1573e47af7
João Paulo Just Peixoto <just1982@gmail.com>
2023-11-06 12:31:30 -0300

    Check if res_data is Hash beforing parsing it

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,7 +3,7 @@ class Result < ApplicationRecord
   belongs_to :task
 
   def res_data_to_json
-    JSON.parse self.res_data
+    self.res_data.is_a?(Hash) ? self.res_data : JSON.parse self.res_data
   end
 
 end
