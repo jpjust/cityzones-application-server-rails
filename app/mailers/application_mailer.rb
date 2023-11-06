be7372dc791af5022689d884d38107586ce14b5c
João Paulo Just Peixoto <just1982@gmail.com>
2023-11-06 11:49:27 -0300

    Add default from address in mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: email_address_with_name('just@just.pro.br', 'CityZones Web')
   layout "mailer"
 end
