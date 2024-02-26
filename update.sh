0e60bf2f362e89db15cc7611f502b26962a42dc4
João Paulo Just Peixoto <just1982@gmail.com>
2024-02-26 11:50:18 -0300

    fix: Usa caminho completo do bundle em update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ echo "Updating app..."
 git pull
 git reset --hard
 #bundle install
-bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+/opt/rbenv/shims/bundle exec rake assets:precompile db:migrate RAILS_ENV=production
 touch tmp/restart.txt
 #passenger-config restart-app $(pwd)
 
