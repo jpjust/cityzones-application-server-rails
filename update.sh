ad4858011240b63c77248ad4b8ca6b44d279e5e5
João Paulo Just Peixoto <just1982@gmail.com>
2024-02-26 15:18:24 -0300

    Adiciona chown em update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ git pull
 git reset --hard
 #bundle install
 /opt/rbenv/shims/bundle exec rake assets:precompile db:migrate RAILS_ENV=production
+chown -R www-data: *
 touch tmp/restart.txt
 #passenger-config restart-app $(pwd)
 
