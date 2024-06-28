92f6dda4a1d0a287f3d3d513ba21abeeae7c225d
João Paulo Just Peixoto <just1982@gmail.com>
2024-06-28 11:28:24 -0300

    feat: Send Open-Elevation response body even on EOFError

--- app/controllers/api/open_elevation_controller.rb
+++ app/controllers/api/open_elevation_controller.rb
@@ -21,7 +21,7 @@ class Api::OpenElevationController < ApplicationController
         render :json => {}, :status => 502
       end
     rescue EOFError
-      render :json => { :results => [] }
+      render :json => res.body
     rescue Timeout::Error, SocketError, Errno::ECONNREFUSED, Errno::ECONNRESET
       render :json => {}, :status => 500
     end
