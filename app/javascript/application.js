18a955651537e4948e6adef1128068e701c92498
João Paulo Just Peixoto <just1982@gmail.com>
2024-06-28 12:33:49 -0300

    fix: Fix JS errors and disable markers drawing when loading GeoJSON files (it was very slow)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+let map = null;
+let tiles = null;
+
+// Variables for new task view
+let task_lat = 0.0;
+let task_lon = 0.0;
+let markers = [];
+let polpoints = [];
+let html_polygon = null;
+let polygon = null;
+let closed = false;
+let html_polpoints = [];
+
+// Variables for results list
+let marker = null;
+let opacity = 0.5;
+let edus_radius = 10;
+let slider = null;
+let slider_value = null;
+let slider_edus = null;
+let slider_edus_value = null;
+let circles = [];
+let edus = [];
